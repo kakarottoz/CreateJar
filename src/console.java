47620de54d0761c6a9f545a374f7a66b573eb15d
kakarottoz <fanzhengjun23@126.com>
2015-10-22 15:57:33 +0800

    哈哈 这是俺第一次提交

---
+++ src/console.java
@@ -0,0 +1,22 @@
+
+public class console {
+	public static void main(String[] args) {
+		try{
+			System.out.println("你现在好吗");
+			Thread.sleep(1000);
+			System.out.println("工作顺利吗");
+			Thread.sleep(1000);
+			System.out.println("我从远方送你的花");
+			Thread.sleep(1000);
+			System.out.println("你收到了吗");
+			Thread.sleep(1000);
+			System.out.println("分手以后的雨季");
+			Thread.sleep(1000);
+			System.out.println("断断续续下不停");
+			
+		}catch(Exception e){
+			
+		}
+		
+	}
+}
